0a91b904a06d6002cafed3778bfc389c810e7ef1
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-02-07 20:34:01 +0100

    Done card creation

--- script.js
+++ script.js
@@ -17,11 +17,23 @@ function loadImages(value) {
   })
     .then((response) => response.json())
     .then((data) => {
-        imageList.innerHTML = '';
-      for (let i = 0; i < 5; i++) {
-        let newImg = document.createElement("img");
-        newImg.src = data.photos[i].src.medium;
-        imageList.appendChild(newImg);
+      imageList.innerHTML = '';
+      for (let i = 0; i < data.photos.length ; i++) {
+        createCard(data, i);
       }
     });
+}
+
+function createCard(data, i){
+  console.log(data.photos[i])
+  imageList.innerHTML += ` 
+  <div class='col-3'> 
+  <div class="card overflow-hidden" id="card-original">
+  <img src="${data.photos[i].src.tiny}" class="card-img-top" alt="${data.photos[i].alt}">
+  <div class="card-body">
+      <p class="card-text"> ${data.photos[i].alt} </p> 
+  </div>
+  </div>
+  </div>
+   `
 }
